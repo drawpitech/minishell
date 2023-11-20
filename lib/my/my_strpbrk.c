7a0408501895e79d3f78e1f12d1f68245316f38b
Clément <clement2104.boillot@gmail.com>
2023-11-20 18:18:13 +0100

    Add my_strpbrk & my_strfind

---
+++ lib/my/my_strpbrk.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** minishell
+** File description:
+** my_strstr
+*/
+
+#include <stddef.h>
+#include <string.h>
+
+#include "my.h"
+
+char *my_strfind(char *str, char c)
+{
+    if (str == NULL)
+        return NULL;
+    for (size_t i = 0; str[i]; i++)
+        if (str[i] == c)
+            return str + i;
+    return NULL;
+}
+
+char *my_strpbrk(char *str, char const *chars)
+{
+    char *ptr = NULL;
+    char *tmp = NULL;
+
+    if (str == NULL || chars == NULL)
+        return NULL;
+    for (size_t i = 0; chars[i]; i++) {
+        tmp = my_strfind(str, chars[i]);
+        if (ptr == NULL || tmp < ptr)
+            ptr = tmp;
+    }
+    return ptr;
+}
