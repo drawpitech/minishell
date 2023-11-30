9d4531f49fe4681f0343c72e572f3fc40c7930f9
Clément <clement2104.boillot@gmail.com>
2023-11-30 14:28:20 +0100

    Add `cd -`

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -13,25 +13,31 @@
 #include "builtins.h"
 
 static
+char const *get_env_var(shell_t *shell, char const *var)
+{
+    char const *res = my_getenv(shell, var);
+
+    if (res == NULL)
+        ret_perror("cd", "%s not set", var);
+    return res;
+}
+
+static
 char const *cd_get_fullpath(shell_t *shell, char const *direction, char *buf)
 {
-    char *home = my_getenv(shell, "HOME");
+    char const *tmp;
 
-    if (direction == NULL) {
-        if (home == NULL) {
-            ret_perror("cd", "HOME not set");
-            return NULL;
-        }
-        return home;
-    }
+    if (direction == NULL)
+        return get_env_var(shell, "HOME");
     if (direction[0] == '~') {
-        if (home == NULL) {
-            ret_perror("cd", "HOME not set");
+        tmp = get_env_var(shell, "HOME");
+        if (tmp == NULL)
             return NULL;
-        }
-        get_fullpath(home, direction + 2, buf);
+        get_fullpath(tmp, direction + 2, buf);
         return buf;
     }
+    if (direction[0] == '-')
+        return get_env_var(shell, "OLDPWD");
     get_fullpath(getcwd(buf, PATH_MAX), direction, buf);
     return buf;
 }
@@ -41,9 +47,11 @@ int builtin_cd(shell_t *shell, char **argv)
     int ret;
     static char buf[PATH_MAX];
     char const *path;
+    char current[PATH_MAX];
 
     if (shell == NULL || argv == NULL)
         return SH_CODE_GENERAL_ERROR;
+    getcwd(current, PATH_MAX);
     path = cd_get_fullpath(shell, argv[1], buf);
     if (path == NULL)
         return SH_CODE_GENERAL_ERROR;
@@ -51,6 +59,7 @@ int builtin_cd(shell_t *shell, char **argv)
     ret = chdir(path);
     if (ret == -1)
         return ret_perror(argv[1], NULL);
+    my_setenv(shell, "OLDPWD", current);
     my_setenv(shell, "PWD", getcwd(buf, PATH_MAX));
     return SH_CODE_SUCCES;
 }
