69126d1dd67aa2fdd67d42b15b11c12315b777be
Clément <clement2104.boillot@gmail.com>
2023-12-07 17:17:49 +0100

    Add redirect append output

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -40,10 +40,15 @@ int cmd_redirect(cmd_stack_t const *stack)
 {
     int fd;
     mode_t mode = S_IRUSR | S_IWUSR;
+    int flag_wr = O_WRONLY | O_CREAT;
 
     switch (stack[1].type) {
         case REDIRECT_OUTPUT:
-            fd = open(stack[2].argv[0], O_WRONLY | O_TRUNC | O_CREAT, mode);
+            fd = open(stack[2].argv[0], flag_wr | O_TRUNC, mode);
+            dup2(fd, STDOUT_FILENO);
+            return fd;
+        case REDIRECT_APPEND_OUTOUT:
+            fd = open(stack[2].argv[0], flag_wr | O_APPEND, mode);
             dup2(fd, STDOUT_FILENO);
             return fd;
         case EXPR:
